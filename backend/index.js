2cb4740603ae3a3618bceca3d5f328821466f32c
LalmaniBytes <lalmani.mishra1508@gmail.com>
2026-02-10 19:36:14 +0530

    Minor syntax issue

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ const corsOptions = {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));x
+      callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true,
